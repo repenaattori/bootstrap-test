cb68ef9af874c5ce398cae1c5eb9ea097c29de88
repenaattori <reimarii@oamk.fi>
2022-11-17 18:31:21 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {Container} from 'react-bootstrap';
+import { Nav } from 'react-bootstrap';
+import { Navbar } from 'react-bootstrap';
+import { NavDropdown } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
 
 function App() {
+
+  //Tähän talletetaan bootstrap-menun valinta (eventKey).
+  const [selection, setSelection] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar bg='dark' variant='dark' expand='lg'>
+          <Container>
+             {/* Lisätään hampparimenu */}
+            <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
+            {/* Navi piilotetaan kapeammassa näytössö (vain hamppari-ikoni näkyy) */}
+            <Navbar.Collapse id='basic-navbar-nav'>
+                 {/* onSelect asettaa menussa valitun itemin eventKeyn */}
+              <Nav className="me-auto" onSelect={sel => setSelection(sel)}>
+                  <Nav.Link eventKey={0}>Home</Nav.Link>
+                  <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                    <NavDropdown.Item eventKey={1}>First</NavDropdown.Item>
+                    <NavDropdown.Item eventKey={2}>Second</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item eventKey={3}>Third</NavDropdown.Item>
+                  </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+          </Container>
+      </Navbar>
+    
+      <h1>Tämän sivun numero on {selection}</h1>
+
     </div>
   );
 }
